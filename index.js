59e76752c2f80cf4819476146d34594b928947eb
hivkosic <117200270+hivkosic@users.noreply.github.com>
2023-06-03 23:42:16 +0200

    verzija s arrayem

--- index.js
+++ index.js
@@ -1,26 +1,25 @@
+
 function add (){
     counter +=1;
     numOfPass.innerHTML=counter;
 }
 
 function save() {
-    numOfRides.textContent+=counter+'-';
-    counter=0;
-    numOfPass.innerHTML=0;
+    todaysRides.push(counter);
+    numOfRides.textContent=`Današnje vožnje (broj putnika): ${todaysRides.join('-')}`;
+    // numOfRides.innerHTML+=counter+'-';
+    // counter=0;
+    // numOfPass.innerHTML=0;
 }
 
 function reset (){
     counter=0;
-    numOfPass.innerHTML=0;
-    numOfRides.textContent='Današnje vožnje (broj putnika): ';
+    todaysRides=[]
+    numOfPass.innerHTML=counter;
+    numOfRides.textContent=`Današnje vožnje (broj putnika): ${counter}`;  
 }
 
 let counter = 0;
-
+let todaysRides=[];
 let numOfPass=document.getElementById('number-of-passengers');
 let numOfRides=document.getElementById('number-of-rides');
-// let addBtn=document.getElementById('add-btn');
-
-// document.getElementById('save-btn');
-// document.getElementById('reset-btn');
-
